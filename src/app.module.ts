19122f5a30477cf5f39f75f89ef9ebf52a5287c3
azin-cn <luckywords@foxmail.com>
2022-11-27 19:09:29 +0800

    refactor: apply *.config.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,33 +4,21 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import envConfig from './config/env';
+// import envConfig from '../env';
+import { getDBConf } from './config/db.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 设置为全局
-      envFilePath: [envConfig.path],
+      // envFilePath: [envConfig.path], 采用 ts 文件配置形式
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (
         configService: ConfigService,
-      ): Promise<TypeOrmModuleOptions> => ({
-        // type: configService.get('DB_TYPE', 'mysql'), error，ts 类型检测
-        type: 'mysql',
-        host: configService.get('DB_HOST', 'localhost'),
-        port: configService.get<number>('DB_PORT', 3306),
-        database: configService.get('DB_NAME', 'web'),
-        username: configService.get('DB_USERNAME', 'root'),
-        password: configService.get<string>('DB_PASSWORD', 'root'),
-        logger: 'debug',
-        entities: [],
-        autoLoadEntities: true,
-        timezone: '+08:00', // 时区
-        synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
-      }),
+      ): Promise<TypeOrmModuleOptions> => getDBConf(),
     }),
   ],
   controllers: [AppController],
---
+++ src/config/db.config.ts
@@ -0,0 +1,30 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const getDBConf = (): TypeOrmModuleOptions => ({
+  // type: configService.get('DB_TYPE', 'mysql'), error，ts 类型检测
+  type: 'mysql',
+  host: getOrdefault('DB_HOST', 'localhost'),
+  port: getOrdefault('DB_PORT', 3306),
+  database: getOrdefault('DB_NAME', 'web'),
+  username: getOrdefault('DB_USERNAME', 'root'),
+  password: getOrdefault('DB_PASSWORD', 'root'),
+  logging: 'all',
+  entities: [],
+  autoLoadEntities: true,
+  timezone: '+08:00', // 时区
+  synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
+});
+export default getDBConf();
+
+function getOrdefault(token: string, defaultValue: number): number;
+function getOrdefault(token: string, defaultValue: string): string;
+function getOrdefault(
+  token: string,
+  defaultValue: number | string,
+): number | string {
+  const value = process.env[token] || defaultValue;
+  if (typeof defaultValue === 'number') {
+    return Number(value);
+  }
+  return value;
+}
