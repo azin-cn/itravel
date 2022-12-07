e42b6be1584f1bbd52242edf6e008f568355fd5b
azin-cn <luckywords@foxmail.com>
2022-12-07 22:47:26 +0800

    feat: add user config

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,7 +1,41 @@
-import { Entity, Column } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
 
+import { Article } from './article.entity';
 
-@Entity()
-export class UserEntity {
-  
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 20 })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ length: 20 })
+  description: string;
+
+  @Column()
+  last_time: number;
+
+  @Column({ default: false })
+  isDeleted: boolean;
+
+  @CreateDateColumn()
+  created_time: number;
+
+  @UpdateDateColumn()
+  updated_time: number;
+
+  // 一对多
+  @OneToMany(() => Article, (article: Article) => article.author)
+  articles: Article[];
 }
