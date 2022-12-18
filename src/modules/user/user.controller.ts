34283a86883d1ca52173a0561fa17c759a2266ab
azin-cn <luckywords@foxmail.com>
2022-12-18 12:03:54 +0800

    feat: add get/post UserModel

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,22 @@
 import { Controller } from '@nestjs/common';
+import { Body, Get, Param, Post } from '@nestjs/common/decorators';
+import { UserService } from './user.service';
+import { User } from 'src/entities/user.entity';
+import { UserCreateDTO } from 'src/dto/user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    private userService: UserService,
+  ) {}
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<User> {
+    return await this.userService.getUserById(id);
+  }
+
+  @Post()
+  async postUser(@Body() user: UserCreateDTO): Promise<UserCreateDTO> {
+    return await this.userService.create(user);
+  }
+}
