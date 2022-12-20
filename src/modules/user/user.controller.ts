0709a3ba633a122b6adaa7a859b7cb5668c360c7
azin-cn <luckywords@foxmail.com>
2022-12-20 09:20:34 +0800

    feat: add ResultVO map

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,14 +3,16 @@ import { Body, Get, Param, Post } from '@nestjs/common/decorators';
 import { UserService } from './user.service';
 import { User } from 'src/entities/user.entity';
 import { UserCreateDTO } from 'src/modules/user/dto/user.dto';
+import { ResultVO } from 'src/shared/vo/ResultVO';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get(':id')
-  async getUserById(@Param('id') id: string): Promise<User> {
-    return await this.userService.getUserById(id);
+  async getUserById(@Param('id') id: string): Promise<ResultVO<User>> {
+    const user = await this.userService.getUserById(id);
+    return ResultVO.info(user);
   }
 
   @Post()
