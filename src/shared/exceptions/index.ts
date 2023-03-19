7b3eac9e43610f756ed730b5757da964c869289b
azin-cn <luckywords@foxmail.com>
2023-03-19 23:46:14 +0800

    chore: remove some define

--- src/shared/exceptions/index.ts
+++ src/shared/exceptions/index.ts
@@ -1,4 +1,5 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { BizException } from './BizException';
 
 export type IException = Record<
   string,
@@ -7,29 +8,3 @@ export type IException = Record<
     errMsg: string;
   }
 >;
-
-
-
-export class BasicException extends HttpException {
-  constructor(
-    errCode: number,
-    errMsg: string = 'Internal server error',
-    httpCode: number = HttpStatus.INTERNAL_SERVER_ERROR, // server_error 500
-  ) {
-    super({ errCode, errMsg }, httpCode);
-  }
-}
-
-// export class BlankException extends HttpException {
-//   constructor(
-//     private errMsg = '未找到对应资源',
-//     private errCode = ExceptionStatus.NOT_FOUND,
-//   ) {
-//     super({ errCode, errMsg }, HttpStatus.OK);
-//   }
-// }
-
-export const registeExcetion =
-  (Exception: IException, key: string) => (errMsg: string, errCode: number) => {
-    Exception[key] = { errCode, errMsg };
-  };
