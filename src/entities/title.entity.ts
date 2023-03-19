2f25f65038ccc383cc1a201ed4f75849ecec08fc
azin-cn <luckywords@foxmail.com>
2023-03-19 23:40:16 +0800

    feat: add title.entity

---
+++ src/entities/title.entity.ts
@@ -0,0 +1,45 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Entity('title')
+export class Title {
+  /**
+   * 头衔 id
+   */
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  /**
+   * 头像名称
+   */
+  @Column()
+  name: string;
+
+  @OneToOne(() => User, (user) => user.title)
+  user: User;
+
+  /**
+   * 是否删除
+   */
+  @Column({ default: false })
+  isDeleted: boolean;
+
+  /**
+   * 创建时间
+   */
+  @CreateDateColumn()
+  createdTime: Date;
+
+  /**
+   * 更新时间
+   */
+  @UpdateDateColumn()
+  updatedTime: Date;
+}
