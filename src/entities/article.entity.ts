545c27c8cdd230e79c204d4372fd342aeac12ceb
azin-cn <luckywords@foxmail.com>
2023-03-19 23:39:54 +0800

    feat: add article.entity

--- src/entities/article.entity.ts
+++ src/entities/article.entity.ts
@@ -5,33 +5,75 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   ManyToOne,
+  ManyToMany,
+  JoinTable,
 } from 'typeorm';
 import { User } from './user.entity';
+import { Tag } from './tag.entity';
 
 @Entity('article')
 export class Article {
+  /**
+   * 文章ID
+   */
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  /**
+   * 文章title
+   */
   @Column({ length: 50 })
   title: string;
 
-  // 注入User，便于使用
-  @ManyToOne((type) => User, (user) => user.articles) // user.articles 关联的属性
+  /**
+   * 文章作者
+   */
+  @ManyToOne((type) => User, (user) => user.articles) // 注入User，user.articles 关联的属性
   author: string; // uuid 为 key
 
+  /**
+   * 文章缩略图
+   */
   @Column({ default: '' })
   thumbUrl: string;
 
+  /**
+   * 文章内容
+   */
   @Column('text')
   content: string;
 
+  /**
+   * 浏览量
+   */
+  @Column()
+  pageViews: number;
+
+  /**
+   * 文章标签，@JoinTable标识字段为被所有者方，当前类为所有者一方
+   * @JoinTable()是@ManyToMany关系所必需的，无论是单项关系还是双向关系，只存在一边
+   * 保存时需要先生成/保存主实体，因为主实体的id需要作为其他实体的索引依赖
+   * 然后生成/设置其他实体中主实体的属性，最后保存其他实体
+   */
+  @ManyToMany(() => Tag, (tag) => tag.id)
+  @JoinTable()
+  tags: Tag[];
+
+  /**
+   * 是否删除
+   */
   @Column({ default: false })
   isDeleted: boolean;
 
+  /**
+   * 创建时间
+   */
   @CreateDateColumn()
   createdTIme: Date;
 
+  /**
+   * 更新时间
+   */
   @UpdateDateColumn()
   updatedTime: Date;
 }
