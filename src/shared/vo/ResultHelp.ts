d2d2651abd6563d171f29cad0b9902d37b35f712
azin-cn <luckywords@foxmail.com>
2023-03-20 11:09:54 +0800

    feat: update some code

--- src/shared/vo/ResultHelp.ts
+++ src/shared/vo/ResultHelp.ts
@@ -3,4 +3,7 @@ export class ResultCode {
   static FAIL = 500;
   static FORBIDDEN = 403;
   static NOT_FOUND = 404;
+  static USERFAIL = 400;
+  static SYSTEMFAIL = 503;
+  static DATABASEFAIL = 510;
 }
