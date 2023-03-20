aeea2f65f1b5026ae5d6b4c61ff91ee137ad178b
azin-cn <luckywords@foxmail.com>
2023-03-20 12:57:41 +0800

    fix: add providers TItleService

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -6,11 +6,12 @@ import { UserService } from './user.service';
 import { Title } from 'src/entities/title.entity';
 import { Article } from 'src/entities/article.entity';
 import { Tag } from 'src/entities/tag.entity';
+import { TitleService } from '../title/title.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Title, Article, Tag])], // dynamic module
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, TitleService],
   exports: [TypeOrmModule],
 })
 export class UserModule {}
