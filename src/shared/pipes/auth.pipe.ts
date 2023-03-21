d3aa031b70f8d54e20aaaa0002b149493e76a7e7
azin-cn <luckywords@foxmail.com>
2023-03-21 17:03:51 +0800

    feat: add auth.pipe

---
+++ src/shared/pipes/auth.pipe.ts
@@ -0,0 +1,18 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { Validator } from './utils';
+import { AuthTypeDTO } from 'src/modules/user/dto/auth.dto';
+
+export class TrasnformAuthPipe implements PipeTransform {
+  async transform(value: AuthTypeDTO): Promise<AuthTypeDTO> {
+    // 默认情况下，query参数是字符串
+    try {
+      value = { ...value, type: Number(value.type) };
+    } catch (error) {
+      throw new BadRequestException('注册类型错误');
+    }
+    value = plainToClass(AuthTypeDTO, value);
+    await Validator.validate(value);
+    return value;
+  }
+}
