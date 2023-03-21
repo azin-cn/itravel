ef3bdf8517f570fd35f3f9f3b9028d20d98a96ec
azin-cn <luckywords@foxmail.com>
2023-03-21 17:04:56 +0800

    feat: update user control and service

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller } from '@nestjs/common';
 import {
   Body,
   Delete,
@@ -6,6 +6,8 @@ import {
   Param,
   Post,
   Put,
+  Query,
+  UseInterceptors,
   UsePipes,
 } from '@nestjs/common/decorators';
 import { UserService } from './user.service';
@@ -14,15 +16,22 @@ import { ResultVO } from 'src/shared/vo/ResultVO';
 import { BizException } from 'src/shared/exceptions/BizException';
 import {
   TransformUserPipe,
-  TransformUserRegisterPipe,
+  TransformUserAuthPipe,
 } from 'src/shared/pipes/user.pipe';
+import { TransformUUIDPipe } from 'src/shared/pipes/uuid.pipe';
+import { AuthTypeDTO } from './dto/auth.dto';
+import { TrasnformAuthPipe } from 'src/shared/pipes/auth.pipe';
+import { UserAuthDTO } from './dto/user.dto';
 
 @Controller('user')
+@UseInterceptors(ClassSerializerInterceptor)
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get(':id')
-  async getUserById(@Param('id') id: string): Promise<ResultVO<User>> {
+  async getUserById(
+    @Param('id', TransformUUIDPipe) id: string,
+  ): Promise<ResultVO<User>> {
     const user = await this.userService.findUserById(id);
     if (user) {
       return ResultVO.info(user);
@@ -30,16 +39,30 @@ export class UserController {
     throw new BizException('用户不存在或已注销');
   }
 
-  @Post()
-  @UsePipes(new TransformUserRegisterPipe())
-  async postUser(@Body() user: User): Promise<User> {
-    console.log(user instanceof User, 'user instanceof User');
-    return await this.userService.create(user);
+  @Post('login')
+  async userLogin(
+    @Body(TransformUserAuthPipe) userDTO: UserAuthDTO,
+    @Query(TrasnformAuthPipe) query: AuthTypeDTO,
+  ): Promise<User> {
+    // 校验用户
+    const { type } = query;
+    console.log(userDTO, query);
+    return await this.userService.login(userDTO, type);
+  }
+
+  @Post('register')
+  async userRegiser(
+    @Body(TransformUserAuthPipe) userDTO: UserAuthDTO,
+    @Query(TrasnformAuthPipe) query: AuthTypeDTO,
+  ) {
+    const { type } = query;
+    return await this.userService.register(userDTO, type);
   }
 
   @Put()
   @UsePipes(new TransformUserPipe())
   async putUser(@Body() user: User): Promise<ResultVO> {
+    console.log(user);
     const { affected } = await this.userService.update(user);
     if (affected === 0) {
       throw new BizException('无此用户，更新失败！');
@@ -48,7 +71,9 @@ export class UserController {
   }
 
   @Delete(':id')
-  async deleteUser(@Param('id') id: string): Promise<ResultVO> {
+  async deleteUser(
+    @Param('id', TransformUUIDPipe) id: string,
+  ): Promise<ResultVO> {
     const { affected } = await this.userService.delete(id);
     if (affected === 0) {
       throw new BizException('无此用户，删除失败！');
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,10 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 import { User } from 'src/entities/user.entity';
 import { Title } from 'src/entities/title.entity';
 import { BizException } from 'src/shared/exceptions/BizException';
+import { AUTH_TYPE } from 'src/shared/constants/auth.constant';
+import { Assert } from 'utils/Assert';
 import { TitleService } from '../title/title.service';
+import { UserAuthDTO } from './dto/user.dto';
+import { isEmail, isMobilePhone } from 'class-validator';
 
 @Injectable()
 export class UserService {
@@ -20,26 +25,35 @@ export class UserService {
     private titleService: TitleService,
   ) {}
 
+  /**
+   * 查找所有用户
+   * @returns
+   */
   async findAll(): Promise<User[]> {
     return this.userRepository.find({
       where: { isDeleted: false },
     });
   }
 
+  /**
+   * 通过ID查找用户
+   * @param id
+   * @returns
+   */
   async findUserById(id: string): Promise<User> {
     return this.userRepository.findOneBy({ id, isDeleted: false });
   }
 
   /**
-   *
+   * 查找所有用户，管理员模式
    * @returns 所有用户，包括已删除
    */
   async findAllAdmin() {
-    return this.userRepository.findAndCount();
+    return this.userRepository.find();
   }
 
   /**
-   *
+   * 通过ID查找用户，管理员模式
    * @param id
    * @returns 寻找用户，包括已删除
    */
@@ -47,7 +61,43 @@ export class UserService {
     return this.userRepository.findOneBy({ id });
   }
 
+  /**
+   * 通过唯一条件查找用户
+   * 唯一条件：用户名、手机、邮箱
+   * @param _user
+   */
+  async findUserByUniqueParam(user: User): Promise<User> {
+    return this.findUserByUniqueParamAdmin(user, false);
+  }
+
+  /**
+   * 通过唯一条件查找用户，管理员模式
+   * @param _user
+   * @param isDeleted
+   * @returns
+   */
+  async findUserByUniqueParamAdmin(_user: User, isDeleted = true) {
+    const { username, phone, email } = _user;
+    const handle = this.userRepository.createQueryBuilder('user');
+    // .select(['username', 'phone', 'email']);
+    handle.where(
+      '(user.username = :username OR user.phone = :phone OR user.email = :email)',
+      { username, phone, email },
+    );
+    if (!isDeleted) {
+      // isDeleted 只在 为false时起效，不代表不查询已删除
+      handle.andWhere('user.is_deleted = :isDeleted', { isDeleted });
+    }
+    return handle.getOne();
+  }
+
+  /**
+   * 创建新用户
+   * @param user
+   * @returns
+   */
   async create(user: User): Promise<User> {
+    // if()
     return this.userRepository.save(user);
   }
 
@@ -62,5 +112,105 @@ export class UserService {
       user.title = title;
     }
     return this.userRepository.update(user.id, user);
+  }
+
+  /**
+   * 实现从 UserAuthDTO 获取 User
+   * @param u
+   * @returns
+   */
+  transformUserFromAuthDTO(u: UserAuthDTO): User {
+    const { account, username, phone, email, password } = u;
+    const user = new User();
+    // 至少需要一个凭证
+    Assert.assertNotNil(account || username || phone || email);
+    user.username = account || username;
+    user.phone = isMobilePhone(account) ? account : phone;
+    user.email = isEmail(account) ? account : email;
+    user.password = password;
+    return user;
+  }
+
+  /**
+   * 注册用户
+   * @param u
+   * @param type
+   * @returns
+   */
+  async register(u: UserAuthDTO, type: number) {
+    const user = this.transformUserFromAuthDTO(u);
+    const userRep = await this.findUserByUniqueParam(user);
+    if (userRep) {
+      throw new BadRequestException('用户已存在');
+    }
+    return this.userRepository.save(user);
+  }
+
+  /**
+   * 登录用户
+   * @param u
+   * @param type
+   * @returns
+   */
+  async login(u: UserAuthDTO, type: number): Promise<User> {
+    // 经过转换后，必然是具有唯一凭证的 User
+    const user = this.transformUserFromAuthDTO(u);
+
+    if (type === AUTH_TYPE.THIRD) {
+      /**
+       * 第三方登录
+       */
+      // TODO: 第三方登录逻辑
+    } else if (type === AUTH_TYPE.MOBILE) {
+      /**
+       * 手机验证码登录
+       */
+      return this.loginWithMOBILE(user, u);
+    } else if (type === AUTH_TYPE.ACCOUNT) {
+      /**
+       * 账户密码登录
+       */
+      Assert.assertNotNil(user.password, '密码不能为空');
+      const userRep = await this.findUserByUniqueParam(user);
+      if (userRep) {
+        /**
+         * 登陆时用户存在，验证密码
+         * 若密码不匹配则抛出异常
+         * 若密码匹配则返回用户信息
+         */
+        const isPwdMatch = bcrypt.compareSync(user.password, userRep.password);
+        if (isPwdMatch) return userRep;
+        throw new BizException('账户或密码错误');
+      } else {
+        /**
+         * 若登录用户不不存在
+         */
+        throw new BizException('用户不存在');
+      }
+    }
+  }
+
+  /**
+   * 手机验证码登录
+   * @param user
+   * @param u
+   * @returns
+   */
+  async loginWithMOBILE(user: User, u: UserAuthDTO): Promise<User> {
+    const { phone, captcha } = u;
+    Assert.isMobilePhone(phone);
+    Assert.assertNotNil(captcha);
+    // TODO: 验证码逻辑
+    return user;
+  }
+
+  /**
+   * 第三方登录
+   * @param user
+   * @param u
+   * @returns
+   */
+  async loginWithThird(user: User, u: UserAuthDTO): Promise<User> {
+    return user;
   }
 }
