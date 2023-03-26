129298efb9db7bf271e268aaa84a698bca321529
azin-cn <luckywords@foxmail.com>
2023-03-26 13:09:23 +0800

    perf: perfect validate logic

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -25,18 +25,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   /**
    * 如果 super 中调用了 passReqToCallback: true，那么此时的参数就变为 req:Req, payload
+   * 注意此时的 args 是未经转化的对象，而不是 class
    * @param user
    */
   async validate(payload: JwtPayload) {
     payload = plainToClass(JwtPayload, payload);
-    console.log(payload.getId());
-    // const user = await this.authService.validateUser(payload);
-    // console.log('==============================');
-    // console.log(user);
-    // if (!user) {
-    //   throw new UnauthorizedException('token不正确');
-    // }
-    // return user;
-    return payload;
+    const user = await this.authService.validateUser(payload);
+    if (!user) {
+      throw new UnauthorizedException('token不正确或已过期');
+    }
+    return user;
   }
 }
