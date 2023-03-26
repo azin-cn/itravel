ba3f44be1b33f77e482ebffbf95282b583aa535e
azin-cn <luckywords@foxmail.com>
2023-03-26 13:08:14 +0800

    feat: add all-exception.filter and order filters

---
+++ src/shared/filters/all-exception.filter.ts
@@ -0,0 +1,24 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { ResultVO } from '../vo/ResultVO';
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json(ResultVO.fail('请求发生错误', status));
+  }
+}
