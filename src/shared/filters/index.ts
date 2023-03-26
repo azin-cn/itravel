034ca400959bd887f0a905f89f52114004134f53
azin-cn <luckywords@foxmail.com>
2023-03-26 23:35:41 +0800

    test: remove all exception filter

--- src/shared/filters/index.ts
+++ src/shared/filters/index.ts
@@ -5,7 +5,7 @@ import { DatabaseQueryExceptionFilter } from './database-exception.filter';
 import { APP_FILTER } from '@nestjs/core';
 
 export const GLOBAL_FILTERS: Provider[] = [
-  AllExceptionFilter, // nestjs 会按照添加的顺序依次调用，所以应该将通用的过滤器放置在前，以防止特殊的全局过滤被覆盖
+  // AllExceptionFilter, // nestjs 会按照添加的顺序依次调用，所以应该将通用的过滤器放置在前，以防止特殊的全局过滤被覆盖
   HttpExceptionFilter,
   DatabaseQueryExceptionFilter,
 ].map((filter) => ({
