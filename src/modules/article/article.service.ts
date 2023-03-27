e6083884c25a736b74aefd09a20650f6870db2f5
azin-cn <luckywords@foxmail.com>
2023-03-27 22:10:59 +0800

    feat: add article service

--- src/modules/article/article.service.ts
+++ src/modules/article/article.service.ts
@@ -1,4 +1,88 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Article } from 'src/entities/article.entity';
+import { Tag } from 'src/entities/tag.entity';
+import { Assert } from 'src/utils/Assert';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ArticleService {}
+export class ArticleService {
+  constructor(
+    @InjectRepository(Article)
+    private articleRepository: Repository<Article>,
+  ) {}
+
+  /**
+   * 通过id查找文章
+   * @param id
+   * @returns
+   */
+  async findArticleById(id: string) {
+    const article = await this.articleRepository.findOneBy({
+      id,
+      isDeleted: false,
+    });
+    Assert.isNotEmptyArticle(article);
+    return article;
+  }
+
+  /**
+   * 通过id查找文章，管理员模式
+   * @param id
+   * @returns
+   */
+  async findArticleByIdAdmin(id: string): Promise<Article> {
+    const article = await this.articleRepository.findOneBy({ id });
+    Assert.isNotEmptyArticle(article);
+    return article;
+  }
+
+  /**
+   * 通过用户id查找文章
+   * @param uid
+   * @returns
+   */
+  async findArticleByUser(uid: string): Promise<Article[]> {
+    const articles = await this.articleRepository.find({
+      where: { author: uid, isDeleted: false },
+    });
+    return articles;
+  }
+
+  /**
+   * 创建文章
+   * @param article
+   * @returns
+   */
+  async create(article: Article): Promise<Article> {
+    /**
+     * 文章的创建并没有过多的限制
+     */
+    return this.articleRepository.create(article);
+  }
+
+  async update(id: string, article: Article): Promise<Article> {
+    /**
+     * 文章的更新无过多的限制
+     */
+    const articleRep = await this.findArticleById(id);
+
+    Assert.assertNotNil(articleRep, '文章不存在或已删除');
+
+    const { affected } = await this.articleRepository.update(id, article);
+
+    /**
+     * 更新后非0断言检查
+     */
+    Assert.isNotZero(affected, '文章更新失败');
+
+    return this.findArticleById(id);
+  }
+
+  // async findArticleByTag(tid: string): Promise<Article[]> {
+  //   const articles = await this.articleRepository.find({
+  //     where: { tags: tid },
+  //   });
+  //   return articles
+  // }
+}
