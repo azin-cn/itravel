494b19999fc00d9bfa72488435f2f7616b84f362
azin-cn <luckywords@foxmail.com>
2023-03-27 22:11:31 +0800

    feat: add article dto and import UserModule

--- src/modules/article/article.module.ts
+++ src/modules/article/article.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Article } from 'src/entities/article.entity';
 import { ArticleController } from './article.controller';
 import { ArticleService } from './article.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Article])],
+  imports: [TypeOrmModule.forFeature([Article]), UserModule],
   controllers: [ArticleController],
   providers: [ArticleService],
   exports: [TypeOrmModule.forFeature([Article]), ArticleService],
---
+++ src/modules/article/dto/article.dto.ts
@@ -0,0 +1,66 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  IsUrl,
+  Length,
+  MinLength,
+} from 'class-validator';
+
+export class ArticleBaseDTO {
+  /**
+   * 文章title
+   */
+  @ApiPropertyOptional({ name: 'username', description: '用户名称' })
+  @IsNotEmpty({ message: '文章标题不为空' })
+  @IsString({ message: '文章标题不是字符串' })
+  @Length(6, 30, {
+    message: '文章标题长度必须为 $constraint1 到 $constraint2 之间',
+  })
+  title: string;
+
+  /**
+   * 文章作者
+   */
+  @IsNotEmpty({ message: '文章作者不存在' })
+  @IsUUID(undefined, { message: '文章作者ID非UUID' })
+  author: string; // uuid 为 key
+
+  /**
+   * 文章缩略图
+   */
+  @IsOptional()
+  @IsUrl(undefined, { message: '缩略图不是正确URL' })
+  thumbUrl: string;
+
+  /**
+   * 文章的简要
+   */
+  @IsOptional()
+  @IsString()
+  @Length(0, 300, {
+    message: '文章简要长度必须在 $constraint1 到 $constraint2 之间',
+  })
+  summary: string;
+
+  /**
+   * 文章内容
+   */
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(1, {
+    message: '文章内容不为空',
+  })
+  content: string;
+}
+
+/**
+ * @class 文章新增 DTO 对象
+ */
+export class ArticleDTO extends ArticleBaseDTO {
+  @IsOptional()
+  @IsUUID(undefined, { message: '文章ID非UUID' })
+  id: string;
+}
