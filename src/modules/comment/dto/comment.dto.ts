bed3531f073f8b6022a4c23ef7b534d07fdc2570
azin-cn <luckywords@foxmail.com>
2023-03-30 14:36:25 +0800

    feat: add api property

--- src/modules/comment/dto/comment.dto.ts
+++ src/modules/comment/dto/comment.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsOptional,
@@ -11,6 +12,7 @@ export class CommentBaseDTO {
   /**
    * 回复人id
    */
+  @ApiProperty({ description: '回复人id' })
   @IsNotEmpty({ message: '回复人 id 不存在' })
   @IsUUID(undefined, { message: '回复用户 id 非 UUID 形式' })
   user: string;
@@ -18,6 +20,7 @@ export class CommentBaseDTO {
   /**
    * 被回复人id
    */
+  @ApiProperty({ description: '被回复人id' })
   @IsNotEmpty({ message: '被回复人 id 不存在' })
   @IsUUID(undefined, { message: '被回复用户 id 非 UUID 形式' })
   toUser: string;
@@ -25,6 +28,7 @@ export class CommentBaseDTO {
   /**
    * 文章id
    */
+  @ApiProperty({ description: '文章id' })
   @IsNotEmpty({ message: '文章 id 不存在' })
   @IsUUID(undefined, { message: '文章 id 非 UUID 形式' })
   article: string;
@@ -32,6 +36,7 @@ export class CommentBaseDTO {
   /**
    * 评论/回复内容
    */
+  @ApiProperty({ description: '评论/回复内容' })
   @IsNotEmpty({ message: '回复内容为空' })
   @IsString()
   @Length(COMMENT_LENGTH.MIN, COMMENT_LENGTH.MAX, {
@@ -42,6 +47,7 @@ export class CommentBaseDTO {
   /**
    * 回复父级，即根品论
    */
+  @ApiProperty({ description: '回复父级，即根品论' })
   @IsOptional()
   @IsUUID(undefined, { message: '父 id 非 UUID 形式' })
   parent: string;
@@ -51,6 +57,7 @@ export class CommentDTO extends CommentBaseDTO {
   /**
    * 评论/回复id
    */
+  @ApiProperty({ description: '评论/回复id' })
   @IsOptional()
   @IsUUID(undefined, { message: '评论 id 非 UUID 形式' })
   id: string;
