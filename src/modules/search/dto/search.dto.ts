b589e0681d2f639d44f6fe2f22b3dd6e74924489
azin-cn <luckywords@foxmail.com>
2023-03-31 17:44:25 +0800

    feat: add SearchResultDTO

--- src/modules/search/dto/search.dto.ts
+++ src/modules/search/dto/search.dto.ts
@@ -1,7 +1,20 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { Article } from 'src/entities/article.entity';
+import { User } from 'src/entities/user.entity';
 
 export class SearchDTO {
   @IsString()
   @IsNotEmpty({ message: '搜索字符串为空' })
   s: string;
+}
+
+export class SearchResultDTO {
+  /**
+   * 符合搜索条件的用户
+   */
+  users: User[];
+  /**
+   * 扶额和搜索条件的文章
+   */
+  articles: Article[];
 }
