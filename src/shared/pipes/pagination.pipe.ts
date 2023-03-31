6d4832e263957c197116102ad78f775e2842db04
azin-cn <luckywords@foxmail.com>
2023-03-31 17:11:24 +0800

    feat: add pagination pipe

---
+++ src/shared/pipes/pagination.pipe.ts
@@ -0,0 +1,31 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { PaginationOptions } from '../dto/pagination.dto';
+import { Validator } from './utils';
+import { plainToClass } from 'class-transformer';
+import { PAGINATION_DEFAULT } from '../constants/pagination.constant';
+
+export class TransformPaginationPipe implements PipeTransform {
+  async transform(value: PaginationOptions, metadata: ArgumentMetadata) {
+    /**
+     * 默认数据
+     */
+    value.limit ||= PAGINATION_DEFAULT.LIMIT;
+    value.page ||= PAGINATION_DEFAULT.PAGE;
+
+    /**
+     * 转换数据格式
+     */
+    value = plainToClass(PaginationOptions, value);
+
+    /**
+     * 校验数据
+     */
+    Validator.validate(value);
+
+    return value;
+  }
+}
