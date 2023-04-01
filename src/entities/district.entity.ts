79e255652e15691e97a0d9a52f815033a9ee5a56
azin-cn <luckywords@foxmail.com>
2023-04-01 10:45:44 +0800

    feat: add district entity

---
+++ src/entities/district.entity.ts
@@ -0,0 +1,55 @@
+import { Column, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { City } from './city.entity';
+import { Region } from './region.entity';
+
+export class District {
+  /**
+   * 县区id
+   */
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  /**
+   * 县区名称
+   */
+  @Column({ type: 'tinytext' })
+  name: string;
+
+  /**
+   * 县区全名/官方名称
+   */
+  @Column({ type: 'tinytext' })
+  fullName: string;
+
+  /**
+   * 高德地图id
+   */
+  @Column({ nullable: true })
+  aid: string;
+
+  /**
+   * 百度地图id
+   */
+  @Column({ nullable: true })
+  bid: string;
+
+  /**
+   * 腾讯地图id
+   */
+  @Column({ nullable: true })
+  tid: string;
+
+  /**
+   * 所属城市
+   */
+  @ManyToOne(() => City, (c) => c.districts)
+  @JoinColumn()
+  city: City;
+
+  /**
+   * 所属的区域
+   */
+  @ManyToOne(() => Region, (r) => r.provinces)
+  @JoinColumn()
+  region: Region;
+}
