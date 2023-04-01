13c997600d1944d3ca11379bb48849e9be61e36f
azin-cn <luckywords@foxmail.com>
2023-04-01 11:22:38 +0800

    feat: add find spot by id

--- src/modules/spot/spot.service.ts
+++ src/modules/spot/spot.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Month } from 'src/entities/month.entity';
 import { Spot } from 'src/entities/spot.entity';
+import { Assert } from 'src/utils/Assert';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -13,6 +14,23 @@ export class SpotService {
     private monthRepository: Repository<Month>,
   ) {}
 
+  /**
+   * 查询指定景点信息
+   * @param id
+   * @returns
+   */
+  async findSpotById(id: string): Promise<Spot> {
+    const spot = await this.spotRepository.findOneBy({ id });
+    console.log(spot);
+    Assert.isNotEmptySpot(spot);
+    return spot;
+  }
+
+  /**
+   * 创建景点，需要认证
+   * @param spot
+   * @returns
+   */
   async create(spot: Spot): Promise<Spot> {
     return this.spotRepository.save(spot);
   }
