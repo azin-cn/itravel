2bede5542eb1f886acfe951d5b7cc69fbc0fe83b
azin-cn <luckywords@foxmail.com>
2023-04-01 10:46:22 +0800

    feat: add spot entity

---
+++ src/entities/spot.entity.ts
@@ -0,0 +1,48 @@
+import {
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { SpotMonth } from './spot-month.entity';
+import { Region } from './region.entity';
+
+export class Spot {
+  /**
+   * id
+   */
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  /**
+   * name
+   */
+  @Column({ type: 'string' })
+  name: string;
+
+  /**
+   * 使用spot-mongth中间表拓展更多信息
+   */
+  @OneToMany(() => SpotMonth, (sm) => sm.spot)
+  spotMonths: SpotMonth[];
+
+  /**
+   * 所属的region
+   */
+  @ManyToOne(() => Region, (r) => r.spots)
+  region: Region;
+
+  /**
+   * 创建时间
+   */
+  @CreateDateColumn({ type: 'timestamp' })
+  createdTime: string;
+
+  /**
+   * 更新时间
+   */
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedTime: string;
+}
