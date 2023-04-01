f13faa208dcc54782d39978d73df6680be0f7b70
azin-cn <luckywords@foxmail.com>
2023-04-01 18:07:35 +0800

    feat: add spot pipe

---
+++ src/shared/pipes/spot.pipe.ts
@@ -0,0 +1,26 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { SpotDTO } from 'src/modules/spot/dto/spot.dto';
+import { Validator } from './utils';
+import { Spot } from 'src/entities/spot.entity';
+
+@Injectable()
+export class TransformSpotDTOPipe implements PipeTransform {
+  constructor() {}
+  async transform(
+    spotDTO: SpotDTO,
+    metadata: ArgumentMetadata,
+  ): Promise<SpotDTO> {
+    /**
+     * 转换数据
+     */
+    spotDTO = plainToClass(SpotDTO, spotDTO);
+
+    /**
+     * 校验数据
+     */
+    await Validator.validate(spotDTO);
+
+    return spotDTO;
+  }
+}
