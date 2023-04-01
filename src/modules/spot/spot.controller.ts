7d4638b5a962441abfba614e6b8ab3b0e4a73cf0
azin-cn <luckywords@foxmail.com>
2023-04-01 11:21:54 +0800

    feat: add get spot by id

--- src/modules/spot/spot.controller.ts
+++ src/modules/spot/spot.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { SpotService } from './spot.service';
+import { TransformUUIDPipe } from 'src/shared/pipes/uuid.pipe';
+import { Assert } from 'src/utils/Assert';
+import { ResultVO } from 'src/shared/vo/ResultVO';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('景点信息')
 @Controller('spot')
 export class SpotController {
   constructor(private spotService: SpotService) {}
 
-  @Get()
-  async getSpotById(id: string) {
-    
+  @ApiOperation({ description: '通过ID获取景点信息' })
+  @ApiParam({ name: 'id' })
+  @Get(':id')
+  async getSpotById(@Param('id', TransformUUIDPipe) id: string) {
+    const spot = await this.spotService.findSpotById(id);
+    Assert.isNotEmptySpot(spot);
+    return ResultVO.success(spot);
   }
 }
