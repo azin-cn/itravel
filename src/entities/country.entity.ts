7d6074888ca224773f91ac13fab20a4416c36e35
azin-cn <luckywords@foxmail.com>
2023-04-01 10:45:18 +0800

    feat: add country entity

---
+++ src/entities/country.entity.ts
@@ -0,0 +1,43 @@
+import { Column, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Province } from './province.entity';
+
+export class Country {
+  /**
+   * 国家id
+   */
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  /**
+   * 国家名称
+   */
+  @Column({ type: 'tinytext' })
+  name: string;
+
+  /**
+   * 国家名称/官方名称
+   */
+  @Column({ type: 'tinytext' })
+  fullName: string;
+
+  /**
+   * 高德地图id
+   */
+  @Column({ nullable: true })
+  aid: string;
+
+  /**
+   * 百度地图id
+   */
+  @Column({ nullable: true })
+  bid: string;
+
+  /**
+   * 腾讯地图id
+   */
+  @Column({ nullable: true })
+  tid: string;
+
+  @OneToMany(() => Province, (p) => p.country)
+  provinces: Province[];
+}
