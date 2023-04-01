cd6a38e6138ecbdd6e97c2b44054ba371dccb091
azin-cn <luckywords@foxmail.com>
2023-04-01 18:06:25 +0800

    feat: update spot dto

--- src/modules/spot/spot.controller.ts
+++ src/modules/spot/spot.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { SpotService } from './spot.service';
 import { TransformUUIDPipe } from 'src/shared/pipes/uuid.pipe';
 import { Assert } from 'src/utils/Assert';
 import { ResultVO } from 'src/shared/vo/ResultVO';
-import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { Month } from 'src/entities/month.entity';
+import { TransformSpotDTOPipe } from 'src/shared/pipes/spot.pipe';
+import { SpotDTO } from './dto/spot.dto';
 
 @ApiTags('景点信息')
 @Controller('spot')
@@ -17,5 +20,20 @@ export class SpotController {
     const spot = await this.spotService.findSpotById(id);
     Assert.isNotEmptySpot(spot);
     return ResultVO.success(spot);
+  }
+
+  @ApiOperation({ description: '获取符合条件的景点' })
+  // @ApiQuery({type:})
+  @Get()
+  async getSpotsByConditions(@Query() conditions) {}
+
+  /**
+   * 更新景点
+   */
+  @ApiOperation({ description: '新增景点' })
+  @Post()
+  async postSpot(@Body(TransformSpotDTOPipe) spotDTO: SpotDTO) {
+    // const spot = this.spotService.create();
+    return ResultVO.success();
   }
 }
