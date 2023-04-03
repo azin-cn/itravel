766c1f66c34194e6289e58f488ec2f9e4a0c4893
azin-cn <luckywords@foxmail.com>
2023-04-03 15:45:35 +0800

    feat: add region module

--- src/modules/region/region.module.ts
+++ src/modules/region/region.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { RegionService } from './region.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { District } from 'src/entities/district.entity';
+import { City } from 'src/entities/city.entity';
+import { Province } from 'src/entities/province.entity';
+import { Country } from 'src/entities/country.entity';
 
 @Module({
-  providers: [RegionService]
+  imports: [TypeOrmModule.forFeature([District, City, Province, Country])],
+  providers: [RegionService],
+  exports: [
+    RegionService,
+    TypeOrmModule.forFeature([District, City, Province, Country]),
+  ],
 })
 export class RegionModule {}
--- src/modules/region/region.service.ts
+++ src/modules/region/region.service.ts
@@ -1,4 +1,75 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { City } from 'src/entities/city.entity';
+import { Country } from 'src/entities/country.entity';
+import { District } from 'src/entities/district.entity';
+import { Province } from 'src/entities/province.entity';
+import { Assert } from 'src/utils/Assert';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class RegionService {}
+export class RegionService {
+  constructor(
+    @InjectRepository(District)
+    private districtRepository: Repository<District>,
+    @InjectRepository(City)
+    private cityRepository: Repository<City>,
+    @InjectRepository(Province)
+    private provinceRepository: Repository<Province>,
+    @InjectRepository(Country)
+    private countryRepository: Repository<Country>,
+  ) {}
+
+  /**
+   * 通过id查找县区
+   * @param id
+   * @returns
+   */
+  async findDistrictById(id: string) {
+    const district = await this.districtRepository.findOne({
+      where: { id },
+      relations: ['city'],
+    });
+    Assert.assertNotNil(district, '县区不存在');
+    return district;
+  }
+
+  /**
+   * 通过id查找城市
+   * @param id
+   * @returns
+   */
+  async findCityById(id: string) {
+    const city = await this.cityRepository.findOne({
+      where: { id },
+      relations: ['province'],
+    });
+    Assert.assertNotNil(city, '城市不存在');
+    return city;
+  }
+  /**
+   * 通过id查找省
+   * @param id
+   * @returns
+   */
+  async findProvinceById(id: string) {
+    const province = await this.provinceRepository.findOne({
+      where: { id },
+      relations: ['country'],
+    });
+    Assert.assertNotNil(province, '省区不存在');
+    return province;
+  }
+  /**
+   * 通过id查找国
+   * @param id
+   * @returns
+   */
+  async findCountryById(id: string) {
+    const country = await this.countryRepository.findOne({
+      where: { id },
+    });
+    Assert.assertNotNil(country, '国区不存在');
+    return country;
+  }
+}
