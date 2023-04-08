c29aedec7cf38e09a8032bc1a5a83855000b4a57
azin-cn <luckywords@foxmail.com>
2023-04-09 00:39:53 +0800

    test: update sql demo

--- src/modules/spot/sql/spot.sql
+++ src/modules/spot/sql/spot.sql
@@ -40,4 +40,95 @@ from spot
 where
     country.name = '中国'
     AND province.`name` = '广东'
-order by weight desc
+order by weight desc;
+
+-- 指定id搜索spot
+
+select
+    spot.id,
+    spot.name,
+    province.id `province_id`,
+    province.name `province_name`,
+    city.id `city_id`,
+    city.name `city_name`
+from spot
+    left join province on province.id = spot.province_id
+    left join city on city.id = spot.city_id
+where
+    spot.id = '00020fcd-9bfe-4299-8ffb-93db0cccb5d8';
+
+-- 测试
+
+select *
+from spot_month
+    left join spot on spot_month.spot_id = spot.id
+where
+    spot.id = '00020fcd-9bfe-4299-8ffb-93db0cccb5d8';
+
+SELECT
+    `spot`.`id` AS `spot_id`,
+    `spot`.`name` AS `spot_name`,
+    `spot`.`description` AS `spot_description`,
+    `spot`.`thumb_url` AS `spot_thumb_url`,
+    `spot`.`is_deleted` AS `spot_is_deleted`,
+    `spot`.`created_time` AS `spot_created_time`,
+    `spot`.`updated_time` AS `spot_updated_time`,
+    `spot`.`country_id` AS `spot_country_id`,
+    `spot`.`province_id` AS `spot_province_id`,
+    `spot`.`city_id` AS `spot_city_id`,
+    `spot`.`district_id` AS `spot_district_id`,
+    `sm`.`id` AS `sm_id`,
+    `sm`.`weight` AS `sm_weight`,
+    `sm`.`is_deleted` AS `sm_is_deleted`,
+    `sm`.`created_time` AS `sm_created_time`,
+    `sm`.`updated_time` AS `sm_updated_time`,
+    `sm`.`spot_id` AS `sm_spot_id`,
+    `sm`.`month_id` AS `sm_month_id`,
+    `sf`.`id` AS `sf_id`,
+    `sf`.`weight` AS `sf_weight`,
+    `sf`.`is_deleted` AS `sf_is_deleted`,
+    `sf`.`created_time` AS `sf_created_time`,
+    `sf`.`updated_time` AS `sf_updated_time`,
+    `sf`.`spot_id` AS `sf_spot_id`,
+    `sf`.`feature_id` AS `sf_feature_id`
+FROM `spot` `spot`
+    LEFT JOIN `spot_month` `sm` ON `sm`.`spot_id` = `spot`.`id` AND (`sm`.`spot_id` = `spot`.`id`)
+    LEFT JOIN `spot_feature` `sf` ON `sf`.`spot_id` = `spot`.`id` AND (`sf`.`spot_id` = `spot`.`id`)
+WHERE
+    `spot`.`is_deleted` = false
+    AND `spot`.`id` = "074bf0bf-5f77-4440-b54e-b0d4386ed54a";
+
+-- PARAMETERS: [false,"074bf0bf-5f77-4440-b54e-b0d4386ed54a"]
+
+SELECT
+    `spot`.`id` AS `spot_id`,
+    `spot`.`name` AS `spot_name`,
+    `spot`.`description` AS `spot_description`,
+    `spot`.`thumb_url` AS `spot_thumb_url`,
+    `spot`.`is_deleted` AS `spot_is_deleted`,
+    `spot`.`created_time` AS `spot_created_time`,
+    `spot`.`updated_time` AS `spot_updated_time`,
+    `spot`.`country_id` AS `spot_country_id`,
+    `spot`.`province_id` AS `spot_province_id`,
+    `spot`.`city_id` AS `spot_city_id`,
+    `spot`.`district_id` AS `spot_district_id`,
+    `sm`.`id` AS `sm_id`,
+    `sm`.`weight` AS `sm_weight`,
+    `sm`.`is_deleted` AS `sm_is_deleted`,
+    `sm`.`created_time` AS `sm_created_time`,
+    `sm`.`updated_time` AS `sm_updated_time`,
+    `sm`.`spot_id` AS `sm_spot_id`,
+    `sm`.`month_id` AS `sm_month_id`,
+    `sf`.`id` AS `sf_id`,
+    `sf`.`weight` AS `sf_weight`,
+    `sf`.`is_deleted` AS `sf_is_deleted`,
+    `sf`.`created_time` AS `sf_created_time`,
+    `sf`.`updated_time` AS `sf_updated_time`,
+    `sf`.`spot_id` AS `sf_spot_id`,
+    `sf`.`feature_id` AS `sf_feature_id`
+FROM `spot` `spot`
+    LEFT JOIN `spot_month` `sm` ON `sm`.`spot_id` = `spot`.`id` AND (`sm`.`spot_id` = `spot`.`id`)
+    LEFT JOIN `spot_feature` `sf` ON `sf`.`spot_id` = `spot`.`id` AND (`sf`.`spot_id` = `spot`.`id`)
+WHERE
+    `spot`.`is_deleted` = ?
+    AND `spot`.`id` = ? -- PARAMETERS: [false,"074bf0bf-5f77-4440-b54e-b0d4386ed54a"]
