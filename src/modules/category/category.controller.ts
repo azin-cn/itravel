b5c08ae5d239cea6226ad60b434aee68aa40d210
azin-cn <luckywords@foxmail.com>
2023-04-14 12:41:23 +0800

    feat: add category controller

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Query } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ResultVO } from 'src/shared/vo/ResultVO';
+import { TransformUUIDPipe } from 'src/shared/pipes/uuid.pipe';
+@ApiTags('分类')
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  @ApiOperation({ summary: '根据用户id获取分类' })
+  @Get('user')
+  async getCagetoriesByUserId(
+    @Query('id', TransformUUIDPipe) id: string,
+  ): Promise<ResultVO> {
+    const categories = await this.categoryService.findCategoriesByUserId(id);
+    return ResultVO.success(categories);
+  }
+}
