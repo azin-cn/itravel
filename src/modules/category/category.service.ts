3706a802c30c00aa90044c4480e99adce1dad0b4
azin-cn <luckywords@foxmail.com>
2023-04-14 12:41:10 +0800

    feat: add category service

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,4 +1,36 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
+import { Category } from 'src/entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+  ) {}
+
+  /**
+   * 根据用户id获取创建的分类
+   * @param userId
+   * @param limit
+   * @returns
+   */
+  async findCategoriesByUserId(
+    userId: string,
+    limit = 10,
+  ): Promise<Category[]> {
+    const qb = this.categoryRepository
+      .createQueryBuilder('category')
+      .where('1=1');
+
+    qb.leftJoin('category.user', 'user')
+      .select(['category.id', 'category.name', 'user.id', 'user.username'])
+      .andWhere('user.id = :userId', { userId })
+      .limit(limit);
+
+    const categories = await qb.getMany();
+
+    return categories;
+  }
+}
