ef098178e380c03ea833410ea44a110e2a17caf1
azin-cn <luckywords@foxmail.com>
2023-04-18 14:39:44 +0800

    feat: add uuid array pipe

--- src/shared/pipes/uuid.pipe.ts
+++ src/shared/pipes/uuid.pipe.ts
@@ -1,6 +1,11 @@
-import { BadRequestException, PipeTransform } from '@nestjs/common';
+import {
+  BadRequestException,
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+} from '@nestjs/common';
 import { isUUID } from 'class-validator';
-
+@Injectable()
 export class TransformUUIDPipe implements PipeTransform {
   transform(value: string) {
     const isuuid = isUUID(value);
@@ -8,5 +13,19 @@ export class TransformUUIDPipe implements PipeTransform {
       throw new BadRequestException('请求ID必须是UUID类型');
     }
     return value;
+  }
+}
+
+@Injectable()
+export class TransformUUIDArrayPipe implements PipeTransform {
+  transform(value: string, metadata: ArgumentMetadata): string[] {
+    const uuids = value.split(',').map((item) => {
+      item = item.trim();
+      if (!isUUID(item)) {
+        throw new BadRequestException('请求ID必须是UUID类型');
+      }
+      return item;
+    });
+    return uuids;
   }
 }
