5294fb6d04204922f72bdb22511b1108bdb75a06
azin-cn <luckywords@foxmail.com>
2023-04-18 14:40:10 +0800

    feat: add get category by ids

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -2,7 +2,10 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ResultVO } from 'src/shared/vo/ResultVO';
-import { TransformUUIDPipe } from 'src/shared/pipes/uuid.pipe';
+import {
+  TransformUUIDArrayPipe,
+  TransformUUIDPipe,
+} from 'src/shared/pipes/uuid.pipe';
 @ApiTags('分类')
 @Controller('category')
 export class CategoryController {
@@ -16,6 +19,15 @@ export class CategoryController {
   ): Promise<ResultVO> {
     const categories =
       await this.categoryService.findCategoriesByWordsAndUserId(s, id);
+    return ResultVO.success(categories);
+  }
+
+  @ApiOperation({ summary: '根据ids获取分类数组' })
+  @Get('ids')
+  async getCategoriesByIds(
+    @Query('ids', TransformUUIDArrayPipe) ids: string[],
+  ): Promise<ResultVO> {
+    const categories = await this.categoryService.findCategoriesByIds(ids);
     return ResultVO.success(categories);
   }
 }
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -11,6 +11,21 @@ export class CategoryService {
   ) {}
 
   /**
+   * 通过ids获取分类数组
+   * @param ids
+   * @returns
+   */
+  async findCategoriesByIds(ids: string[]): Promise<Category[]> {
+    const qb = this.categoryRepository
+      .createQueryBuilder('category')
+      .where('1=1')
+      .andWhere('category.id IN (:...ids)', { ids })
+      .select(['category.id', 'category.name']);
+    const categories = await qb.getMany();
+    return categories;
+  }
+
+  /**
    * 根据用户id获取创建的分类
    * @param userId
    * @param limit
