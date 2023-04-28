5a6b885a0a51a944a579707176864594bc580b2b
azin-cn <luckywords@foxmail.com>
2023-04-28 19:44:29 +0800

    feat: add images field

--- src/shared/pipes/article.pipe.ts
+++ src/shared/pipes/article.pipe.ts
@@ -11,12 +11,14 @@ import { Article } from 'src/entities/article.entity';
 import { UserService } from 'src/modules/user/user.service';
 import { Assert } from 'src/utils/Assert';
 import { SpotService } from 'src/modules/spot/spot.service';
+import { CategoryService } from 'src/modules/category/category.service';
 
 @Injectable()
 export class TransformArticlePipe implements PipeTransform {
   constructor(
     private userService: UserService,
     private spotService: SpotService,
+    private categoryService: CategoryService,
   ) {}
   async transform(articleDTO: ArticleDTO, metadata: ArgumentMetadata) {
     /**
@@ -31,9 +33,10 @@ export class TransformArticlePipe implements PipeTransform {
 
     const article = new Article();
     article.title = articleDTO.title?.trim();
-    article.content = articleDTO.content?.trim();
+    article.content = articleDTO.content;
     article.summary = articleDTO.summary?.trim();
     article.thumbUrl = articleDTO.thumbUrl?.trim();
+    article.images = articleDTO.images;
     const author = await this.userService.findUserById(articleDTO.author);
     Assert.isNotEmptyUser(author, '用户不存在或已注销');
     article.author = author;
