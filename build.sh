d1f56cfccf1dc4d71d1a5e0114eda5496f36af20
azin-cn <luckywords@foxmail.com>
2023-05-01 12:41:41 +0800

    chore: update build shell

--- build.sh
+++ build.sh
@@ -8,12 +8,18 @@ sudo rm -rf ./www/*
 # 解压，跳过头层
 sudo tar -zxvf ./tmp/dist.tar.gz -C ./www --strip-components=1
 
+# 复制 config
+cp -r ./config ./www
+
 # 构建 docker
 cd ./www
 sudo docker build -t dev-itravel .
 
-# 停止并删除
-sudo docker stop dev-itravel && sudo docker rm dev-itravel
+# 停止并删除，当容器不存在时会报错，需要加以判断
+if [ "$(docker ps -a -q -f name=dev-itravel)" ]; then
+  sudo docker stop dev-itravel
+  sudo docker rm dev-itravel
+fi
 
 # 运行
 sudo bash /root/docker-script/docker-dev-itravel.sh
--- docker.sh
+++ docker.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
+set -e
+cd /opt/docker/dev-itravel
+
+# 复制 config
+cp -r ./config ./www
+
 # 构建 docker
 cd ./www
 sudo docker build -t dev-itravel .
 
-# 停止并删除
-sudo docker stop dev-itravel && sudo docker rm dev-itravel
+# 停止并删除，当容器不存在时会报错，需要加以判断
+if [ "$(docker ps -a -q -f name=dev-itravel)" ]; then
+  sudo docker stop dev-itravel
+  sudo docker rm dev-itravel
+fi
 
 # 运行
 sudo bash /root/docker-script/docker-dev-itravel.sh
