751300f8411e7c245a218ae740e5a2a7260dcd42
azin-cn <luckywords@foxmail.com>
2023-05-02 17:35:34 +0800

    fix: update permissions

--- build.sh
+++ build.sh
@@ -27,9 +27,11 @@ fi
 
 # 删除无用 none 镜像
 if [ "$(docker images -f "dangling=true" -q)" ]; then
-  docker rmi $(docker images -f "dangling=true" -q)
-  docker images | grep '<none>'
+  sudo docker rmi $(docker images -f "dangling=true" -q)
+  sudo docker images | grep '<none>'
 fi
 
 # 运行
 sudo bash /root/docker-script/docker-dev-itravel.sh
+
+ps -aux |grep -v 
