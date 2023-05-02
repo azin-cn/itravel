fd71834fccffadbef0d450c731da95b2ad72b45b
azin-cn <luckywords@foxmail.com>
2023-05-02 17:40:25 +0800

    chore: add rmi none images

--- build.sh
+++ build.sh
@@ -10,21 +10,26 @@ sudo rm -rf ./www/*
 sudo tar -zxvf ./tmp/dist.tar.gz -C ./www --strip-components=1
 
 # 移动原有的upload，覆盖
-rsync -a ./www/upload ./
+sudo rsync -a ./www/upload ./
 
 # 复制 config
-cp -r ./config ./www
+sudo cp -r ./config ./www
 
 # 构建 docker
 cd ./www
 sudo docker build -t dev-itravel .
+
+# 删除无用 none 镜像
+if [ "$(sudo docker images -f "dangling=true" -q)" ]; then
+  sudo docker rmi $(docker images -f "dangling=true" -q)
+  sudo docker images | grep '<none>'
+fi
 
 # 停止并删除，当容器不存在时会报错，需要加以判断
 if [ "$(docker ps -a -q -f name=^dev-itravel$)" ]; then
   sudo docker stop dev-itravel
   sudo docker rm dev-itravel
 fi
-
 
 # 运行
 sudo bash /root/docker-script/docker-dev-itravel.sh
