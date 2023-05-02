fdea3bee23f2a5f485ec57b61f8c549eddaaa7f3
azin-cn <luckywords@foxmail.com>
2023-05-02 17:43:49 +0800

    chore: change shell order

--- build.sh
+++ build.sh
@@ -19,15 +19,16 @@ sudo cp -r ./config ./www
 cd ./www
 sudo docker build -t dev-itravel .
 
-# 删除无用 none 镜像
-if [ "$(sudo docker images -f "dangling=true" -q)" ]; then
-  sudo docker rmi $(docker images -f "dangling=true" -q)
-fi
 
 # 停止并删除，当容器不存在时会报错，需要加以判断
 if [ "$(docker ps -a -q -f name=^dev-itravel$)" ]; then
   sudo docker stop dev-itravel
   sudo docker rm dev-itravel
+fi
+
+# 删除无用 none 镜像，必须要无任何使用的情况下
+if [ "$(sudo docker images -f "dangling=true" -q)" ]; then
+  sudo docker rmi $(docker images -f "dangling=true" -q)
 fi
 
 # 运行
