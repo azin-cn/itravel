5b4c19764f831b6fb4f0d1d3923cff1931e7e206
azin-cn <luckywords@foxmail.com>
2023-05-02 08:41:57 +0800

    feat: add api auth prefix

--- src/config/common.config.ts
+++ src/config/common.config.ts
@@ -2,4 +2,5 @@ import { getOrdefault } from './utils';
 
 export const getCommonConf = () => ({
   hostname: getOrdefault('CUSTOMER_HOSTNAME', 'localhost'),
+  authPrefix: getOrdefault('AUTH_HTTP_PREFIX', 'localhost'),
 });
