807159617fce7429d154e360a609e2b6b82b56ed
azin-cn <luckywords@foxmail.com>
2023-05-06 15:29:29 +0800

    feat: update admin service

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { ArticleModule } from '../article/article.module';
+import { SpotModule } from '../spot/spot.module';
+import { CommentModule } from '../comment/comment.module';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService]
+  imports: [SpotModule, ArticleModule, CommentModule],
+  providers: [AdminService],
 })
 export class AdminModule {}
