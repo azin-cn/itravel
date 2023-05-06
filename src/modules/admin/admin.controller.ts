83c0f4d77e6cc50f8d3538cfffeff86e36617006
azin-cn <luckywords@foxmail.com>
2023-05-06 15:29:13 +0800

    feat: update admin controller

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ResultVO } from 'src/shared/vo/ResultVO';
+import { AdminService } from './admin.service';
 
+@ApiTags('Admin')
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  constructor(private adminService: AdminService) {}
+
+  @ApiOperation({ summary: '获取Counter数据' })
+  @Get('workspace/counter')
+  async getAdminWorkspaceCounter(): Promise<ResultVO> {
+    const counter = await this.adminService.findWorkspaceCounter();
+    return ResultVO.success(counter);
+  }
+  @ApiOperation({ summary: '获取Workspace总数据' })
+  @Get('workspace')
+  async getAdminWorkspace(): Promise<ResultVO> {
+    const res = await this.adminService.findWorkspaceData();
+    return ResultVO.success(res);
+  }
+}
