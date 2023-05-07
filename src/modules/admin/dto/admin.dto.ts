d3e06b8455f740fd2e8dcad27bd3b8c2f97aa448
azin-cn <luckywords@foxmail.com>
2023-05-07 19:29:38 +0800

    feat: add spot search dto

--- src/modules/admin/dto/admin.dto.ts
+++ src/modules/admin/dto/admin.dto.ts
@@ -1,1 +1,24 @@
+import { IsDate, IsOptional, IsString } from 'class-validator';
+
 export class AdminBaseDTO {}
+
+export class SpotSearchDTO {
+  @IsOptional()
+  id: string;
+
+  @IsOptional()
+  name: string;
+
+  @IsOptional()
+  region: string;
+
+  @IsOptional()
+  @IsString()
+  // @IsDate({ message: '时间格式不正确' })
+  create_date_before: string;
+
+  @IsOptional()
+  @IsString()
+  // @IsDate({ message: '时间格式不正确' })
+  create_date_after: string;
+}
